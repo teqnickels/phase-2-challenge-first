25ce9d9f9f99bf9647b2d9dd9a0b5a1696365f80
Debrena McEwen <debrenamcewen@gmail.com>
2017-08-01 18:57:09 -0700

    add error handling

--- part-1/src/date.js
+++ part-1/src/date.js
@@ -1,22 +1,37 @@
 
-var date = {
+var dateObj = {
   format: function date(date) {
-    var dateArr = date.split( '/' )
-    for (var i = 0; i < dateArr.length; i++) {
-      dateArr[i] = parseInt( dateArr[i] )
-    }
+    if(!parseInt(date)){
+      throw "Invalid Entry " + date
+    }else{
+      var dateArr = date.split( '/' )
+      for (var i = 0; i < dateArr.length; i++) {
+        dateArr[i] = parseInt( dateArr[i] )
+      }
+  }
     var year = dateArr.pop();
     dateArr.unshift(year)
     return dateArr
   },
   monthOfTheYear: function month( date ) {
-    var formattedDate = this.format(date)
-    var date = new Date(date);
-    var dateString = date.toString()
-    var monthArr = dateString.split(' ')
-    
-    return monthArr[1]
+    try{
+      var formattedDate = this.format(date)
+      var date = new Date(date);
+      var dateString = date.toString()
+      var monthArr = dateString.split(' ')
+      var month = monthArr[1]
+      console.log(month);
+      return month
+    }
+    catch(error) {
+      console.log(error);
+    }
   }
 }
-date.monthOfTheYear( '7/12/1981' )
+dateObj.monthOfTheYear( '7/12/1981' )
+dateObj.monthOfTheYear( 'hello')
+dateObj.monthOfTheYear( 'July 5th 1981')
+dateObj.monthOfTheYear( '8/20/1999' )
+
+
 // module.exports { funcs }
