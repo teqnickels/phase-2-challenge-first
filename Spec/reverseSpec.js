8c6381564a4d2b457bee63144a6cbb618eccb3a7
Debrena McEwen <debrenamcewen@gmail.com>
2017-08-02 00:02:47 -0700

    add test progress

---
+++ Spec/reverseSpec.js
@@ -0,0 +1,21 @@
+import chai, { expect } from 'chai'
+import chaiChange from 'chai-change'
+import { func } from '../part-2/reverse'
+
+chai.use(chaiChange)
+
+describe('func', () => {
+  'use strict'
+
+  it('exists', () => {
+    expect( { func } ).to.be.a('object')
+  })
+
+  context( 'func.reverseSentence()', () => {
+  it( 'takes a string and reverses it if there is more than one word in the string',() => {
+
+    expect( func.reverseSentence('I am testing this string.') ).to.equal( 'testing am I' )
+      })
+    })
+
+})
